16f9388619510b5922801e57a6d2a97b40518330
iamdooh <myname520@naver.com>
2021-10-15 16:08:18 +0900

    1-webpack/2-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,25 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+          publicPath: './dist',
+          limit: 10000 // 10kb
+        }
+      }
+    ]
   }
 }
